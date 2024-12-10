65817ee2a6c18cbc22ed996348fbb65cc02633a4
potakaaa <helbirog@gmail.com>
2024-12-11 03:14:04 +0800

    added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/regressionify",
+  base: "/regressionify/client",
   css: {
     postcss: {
       plugins: [tailwindcss()],

a9b1eb460f4710f230c98dcd0731ac0bd789e4c6
potakaaa <helbirog@gmail.com>
2024-12-14 23:27:19 +0800

    changed title and logo

--- client/src/pages/LoadingSpinner.tsx
+++ client/src/pages/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import "./LoadingSpinnerStyle.css"; // This imports the CSS file
 
 const LoadingScreen: React.FC = () => {
   return (
-    <div className="absolute size-full bg-black bg-opacity-35">
+    <div className="absolute size-full bg-black bg-opacity-35 min-h-screen w-screen">
       <div className="absolute size-full flex justify-center items-center">
         <span id="loader"></span>
       </div>
